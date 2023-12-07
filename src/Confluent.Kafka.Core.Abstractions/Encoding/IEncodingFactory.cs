16404efad0e47d4f02a19d044ef2b5f4a4f0fd16
Fernando Luiz de Lima <fernando.lima@warren.com.br>
2023-12-06 22:05:24 -0300

    Added encoding abstractions.

---
+++ src/Confluent.Kafka.Core.Abstractions/Encoding/IEncodingFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Confluent.Kafka.Core.Encoding
+{
+    using System.Text;
+
+    public interface IEncodingFactory
+    {
+        static Encoding Create() => new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: false);
+    }
+}
