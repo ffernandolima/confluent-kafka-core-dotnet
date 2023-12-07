a4f4dd2abfd34d990cf25fe08264db75ef5b2e7b
Fernando Luiz de Lima <fernando.lima@warren.com.br>
2023-12-06 22:05:50 -0300

    Added retry abstractions.

---
+++ src/Confluent.Kafka.Core.Abstractions/Retry/IRetryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Confluent.Kafka.Core.Retry
+{
+    public interface IRetryHandler<TKey, TValue>
+    {
+        void TryHandle(
+            Action<CancellationToken> executeAction,
+            Action<Exception, TimeSpan, int> onRetryAction = null,
+            CancellationToken cancellationToken = default);
+
+        Task TryHandleAsync(
+            Func<CancellationToken, Task> executeAction,
+            Action<Exception, TimeSpan, int> onRetryAction = null,
+            CancellationToken cancellationToken = default);
+    }
+}
