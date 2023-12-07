976008b207faad6e5f3ae01d54c1864ee66977ab
Fernando Luiz de Lima <fernando.lima@warren.com.br>
2023-12-06 22:10:57 -0300

    Added OpenTelemetry implementation.

---
+++ src/Confluent.Kafka.Core.OpenTelemetry/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenTelemetry.Trace
+{
+    public static class TracerProviderBuilderExtensions
+    {
+        public static TracerProviderBuilder AddKafkaInstrumentation(this TracerProviderBuilder builder)
+            => builder.AddSource("Confluent.Kafka.Core");
+    }
+}
