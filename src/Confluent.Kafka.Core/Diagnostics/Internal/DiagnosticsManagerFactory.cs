1995bacd2de3be6c3c30b4a59ab1f98c5e7db037
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2023-12-12 22:53:10 -0300

    Added a centralized way to create DiagnosticsManager instances.

---
+++ src/Confluent.Kafka.Core/Diagnostics/Internal/DiagnosticsManagerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confluent.Kafka.Core.Diagnostics.Internal
+{
+    internal static class DiagnosticsManagerFactory
+    {
+        public static IDiagnosticsManager GetDiagnosticsManager(IServiceProvider serviceProvider, bool enableDiagnostics)
+        {
+            var diagnosticsManager = !enableDiagnostics
+                ? serviceProvider?.GetKeyedService<IDiagnosticsManager>(nameof(NoopDiagnosticsManager)) ?? NoopDiagnosticsManager.Instance
+                : serviceProvider?.GetKeyedService<IDiagnosticsManager>(nameof(KafkaDiagnosticsManager)) ?? KafkaDiagnosticsManager.Instance;
+
+            return diagnosticsManager;
+        }
+    }
+}
