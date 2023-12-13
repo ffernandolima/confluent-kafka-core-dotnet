3f53cda2af9e11286f47cd4df78d0abe65954b24
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2023-12-12 22:55:55 -0300

    Added DiagnosticsManager as member of Producer and Consumer options.

--- src/Confluent.Kafka.Core/Consumer/Internal/KafkaConsumerOptions.cs
+++ src/Confluent.Kafka.Core/Consumer/Internal/KafkaConsumerOptions.cs
@@ -1,4 +1,5 @@
-﻿using Confluent.Kafka.Core.Retry;
+﻿using Confluent.Kafka.Core.Diagnostics;
+using Confluent.Kafka.Core.Retry;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,15 +14,17 @@ namespace Confluent.Kafka.Core.Consumer.Internal
 
         public IKafkaConsumerConfig ConsumerConfig { get; init; }
 
+        public IDiagnosticsManager DiagnosticsManager { get; init; }
+
         public IDeserializer<TKey> KeyDeserializer { get; init; }
 
         public IDeserializer<TValue> ValueDeserializer { get; init; }
 
-        public Func<IKafkaConsumer<TKey, TValue>, object> ConsumerIdHandler { get; init; }
-
         public Func<TValue, object> MessageIdHandler { get; init; }
 
         public IRetryHandler<TKey, TValue> RetryHandler { get; init; }
+
+        public Func<IKafkaConsumer<TKey, TValue>, object> ConsumerIdHandler { get; init; }
 
         public IEnumerable<IKafkaConsumerInterceptor<TKey, TValue>> Interceptors { get; init; }
     }
--- src/Confluent.Kafka.Core/Producer/Internal/KafkaProducerOptions.cs
+++ src/Confluent.Kafka.Core/Producer/Internal/KafkaProducerOptions.cs
@@ -1,4 +1,5 @@
-﻿using Confluent.Kafka.Core.Retry;
+﻿using Confluent.Kafka.Core.Diagnostics;
+using Confluent.Kafka.Core.Retry;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,15 +14,17 @@ namespace Confluent.Kafka.Core.Producer.Internal
 
         public IKafkaProducerConfig ProducerConfig { get; init; }
 
+        public IDiagnosticsManager DiagnosticsManager { get; init; }
+
         public ISerializer<TKey> KeySerializer { get; init; }
 
         public ISerializer<TValue> ValueSerializer { get; init; }
 
-        public Func<IKafkaProducer<TKey, TValue>, object> ProducerIdHandler { get; init; }
-
         public Func<TValue, object> MessageIdHandler { get; init; }
 
         public IRetryHandler<TKey, TValue> RetryHandler { get; init; }
+
+        public Func<IKafkaProducer<TKey, TValue>, object> ProducerIdHandler { get; init; }
 
         public IEnumerable<IKafkaProducerInterceptor<TKey, TValue>> Interceptors { get; init; }
     }
