deeb7c329176e5ad2464053ea4e31b60fdce68ab
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2023-12-16 18:23:28 -0300

    Set context internal.

--- src/Confluent.Kafka.Core/Consumer/KafkaConsumerInterceptorContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Confluent.Kafka.Core.Consumer
-{
-    public sealed class KafkaConsumerInterceptorContext<TKey, TValue> : IKafkaConsumerInterceptorContext<TKey, TValue>
-    {
-        public ConsumeResult<TKey, TValue> ConsumeResult { get; init; }
-        public IKafkaConsumerConfig ConsumerConfig { get; init; }
-    }
-}
