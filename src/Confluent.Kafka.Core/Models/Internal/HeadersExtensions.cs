1d68f696354488962e9ba4c5899168925a2e4778
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2023-12-16 20:00:13 -0300

    Added HeadersExtensions to manipulate Headers easily.

---
+++ src/Confluent.Kafka.Core/Models/Internal/HeadersExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Confluent.Kafka.Core.Models.Internal
+{
+    using System.Text;
+
+    internal static class HeadersExtensions
+    {
+        public static IDictionary<string, string> ToDictionary(this Headers headers, Encoding encoding = null)
+        {
+            if (headers is null)
+            {
+                throw new ArgumentNullException(nameof(headers), $"{nameof(headers)} cannot be null.");
+            }
+
+            var kafkaHeaders = new KafkaHeaders(headers, encoding);
+
+            return kafkaHeaders;
+        }
+    }
+}
