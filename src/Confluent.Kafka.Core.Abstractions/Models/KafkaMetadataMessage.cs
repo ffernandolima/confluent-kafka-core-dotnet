aa3114ed53f7b9ebcce3ed835227c17aa7c81185
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-01-09 16:01:53 -0300

    Defined a metadata message.

---
+++ src/Confluent.Kafka.Core.Abstractions/Models/KafkaMetadataMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Confluent.Kafka.Core.Models
+{
+    public class KafkaMetadataMessage : IMessageValue
+    {
+        private Guid _id;
+        public Guid Id
+        {
+            get => _id;
+            set
+            {
+                if (!_id.Equals(Guid.Empty))
+                {
+                    throw new InvalidOperationException($"{nameof(Id)} has already been assigned.");
+                }
+                _id = value;
+            }
+        }
+
+        public object SourceId { get; init; }
+        public string SourceTopic { get; init; }
+        public string SourceGroupId { get; init; }
+        public Partition SourcePartition { get; init; }
+        public Offset SourceOffset { get; init; }
+        public byte[] SourceKey { get; init; }
+        public byte[] SourceMessage { get; init; }
+        public string SourceKeyType { get; init; }
+        public string SourceMessageType { get; init; }
+        public ErrorCode ErrorCode { get; init; }
+        public string Reason { get; init; }
+    }
+}
