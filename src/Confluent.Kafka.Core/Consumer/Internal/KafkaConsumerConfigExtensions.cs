11b8c4d65a5561b8c65fafc9b119409360a76ec4
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-01-17 15:52:36 -0300

    Added extensions for IKafkaConsumerConfig.

---
+++ src/Confluent.Kafka.Core/Consumer/Internal/KafkaConsumerConfigExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Confluent.Kafka.Core.Consumer.Internal
+{
+    internal static class KafkaConsumerConfigExtensions
+    {
+        public static bool HasTopicSubscriptions(this IKafkaConsumerConfig consumerConfig)
+            => consumerConfig is not null &&
+               consumerConfig.TopicSubscriptions is not null &&
+               consumerConfig.TopicSubscriptions.Any(topic => !string.IsNullOrWhiteSpace(topic));
+
+        public static bool HasPartitionAssignments(this IKafkaConsumerConfig consumerConfig)
+            => consumerConfig is not null &&
+               consumerConfig.PartitionAssignments is not null &&
+               consumerConfig.PartitionAssignments.Any(assignment => assignment is not null);
+    }
+}
