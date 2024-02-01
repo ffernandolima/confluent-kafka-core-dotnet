69bb9aa24de806641cec02533e9ae4b631a9b7e9
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-02-01 18:11:06 -0300

    Added Message extensions.

---
+++ src/Confluent.Kafka.Core/Models/Internal/MessageExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Confluent.Kafka.Core.Models.Internal
+{
+    internal static class MessageExtensions
+    {
+        public static void EnsureDefaultMetadata<TKey, TValue>(this Message<TKey, TValue> message)
+        {
+            if (message is null)
+            {
+                throw new ArgumentNullException(nameof(message), $"{nameof(message)} cannot be null.");
+            }
+
+            message.Headers ??= new Headers();
+
+            if (message.Timestamp == Timestamp.Default)
+            {
+                message.Timestamp = new Timestamp(dateTime: DateTime.UtcNow);
+            }
+        }
+    }
+}
