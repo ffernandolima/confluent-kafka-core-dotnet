b5e94bee0dc537e2a76b018a5b073539795dadf5
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-02-01 18:11:56 -0300

    Added a DeliveryReport factory to centralize the creation of default reports.

---
+++ src/Confluent.Kafka.Core/Producer/Internal/DeliveryReportFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace Confluent.Kafka.Core.Producer.Internal
+{
+    internal static class DeliveryReportFactory
+    {
+        public static DeliveryReport<TKey, TValue> CreateDefault<TKey, TValue>(
+            TopicPartition partition,
+            Message<TKey, TValue> message)
+        {
+            var deliveryReport = new DeliveryReport<TKey, TValue>
+            {
+                Message = message,
+                Error = new Error(ErrorCode.NoError),
+                Status = PersistenceStatus.PossiblyPersisted,
+                TopicPartitionOffset = new TopicPartitionOffset(partition, Offset.Unset)
+            };
+
+            return deliveryReport;
+        }
+    }
+}
