3be013bd7ce080dcb80c7c8a6df92d2a302e5b3d
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-02-01 18:10:34 -0300

    Added a ProduceResult object to make it easier to parse production results when producing messages through the sync Produce method.

---
+++ src/Confluent.Kafka.Core/Producer/Internal/ProduceResult``.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Confluent.Kafka.Core.Producer.Internal
+{
+    internal sealed class ProduceResult<TKey, TValue>
+    {
+        public bool DeliveryHandled { get; private set; }
+        public DeliveryReport<TKey, TValue> DeliveryReport { get; private set; }
+        public bool Faulted => DeliveryReport.Error!.IsError;
+
+        public ProduceResult(TopicPartition partition, Message<TKey, TValue> message)
+        {
+            DeliveryReport = DeliveryReportFactory.CreateDefault(partition, message);
+        }
+
+        public void Complete(DeliveryReport<TKey, TValue> deliveryReport)
+        {
+            if (deliveryReport is null)
+            {
+                throw new ArgumentNullException(nameof(deliveryReport), $"{nameof(deliveryReport)} cannot be null.");
+            }
+
+            DeliveryHandled = true;
+            DeliveryReport = deliveryReport;
+        }
+    }
+}
