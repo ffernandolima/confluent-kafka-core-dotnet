1c03a39b37e9c8193c4069074610866bdce472bf
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-02-16 12:58:51 -0300

    Added a new extension method for getting the message id from the current message.

--- src/Confluent.Kafka.Core/Models/Internal/MessageExtensions.cs
+++ src/Confluent.Kafka.Core/Models/Internal/MessageExtensions.cs
@@ -18,5 +18,17 @@ namespace Confluent.Kafka.Core.Models.Internal
                 message.Timestamp = new Timestamp(dateTime: DateTime.UtcNow);
             }
         }
+
+        public static object GetId<TKey, TValue>(this Message<TKey, TValue> message, Func<TValue, object> handler)
+        {
+            if (message is null)
+            {
+                throw new ArgumentNullException(nameof(message), $"{nameof(message)} cannot be null.");
+            }
+
+            var messageId = handler?.Invoke(message.Value);
+
+            return messageId;
+        }
     }
 }
