209d23319d5fd9de321b306783c17b27fd4faa65
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-02-21 20:37:57 -0300

    Removed unused using.

--- src/Confluent.Kafka.Core/Consumer/KafkaConsumerConfig.cs
+++ src/Confluent.Kafka.Core/Consumer/KafkaConsumerConfig.cs
@@ -4,7 +4,6 @@ using Confluent.Kafka.Core.Retry;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Threading;
 
 namespace Confluent.Kafka.Core.Consumer
