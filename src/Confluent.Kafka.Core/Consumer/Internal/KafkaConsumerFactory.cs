250bf73b089eaf145139e58d24a1ef3f572b89e3
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-06-16 23:32:06 -0300

    Added Consumer/Producer factories.

---
+++ src/Confluent.Kafka.Core/Consumer/Internal/KafkaConsumerFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Confluent.Kafka.Core.Consumer.Internal
+{
+    internal static class KafkaConsumerFactory
+    {
+        public static IKafkaConsumer<TKey, TValue> GetOrCreateConsumer<TKey, TValue>(
+            IServiceProvider serviceProvider,
+            ILoggerFactory loggerFactory,
+            Action<IServiceProvider, IKafkaConsumerBuilder<TKey, TValue>> configureConsumer,
+            object consumerKey)
+        {
+            var consumerbuilder = serviceProvider?.GetKeyedService<IKafkaConsumerBuilder<TKey, TValue>>(consumerKey) ??
+                new KafkaConsumerBuilder<TKey, TValue>()
+                    .WithConsumerKey(consumerKey)
+                    .WithLoggerFactory(
+                        loggerFactory ??
+                        serviceProvider?.GetService<ILoggerFactory>())
+                    .WithServiceProvider(serviceProvider);
+
+            configureConsumer?.Invoke(serviceProvider, consumerbuilder);
+
+#if NETSTANDARD2_0_OR_GREATER
+            var consumer = consumerbuilder.Build().ToKafkaConsumer();
+#else
+            var consumer = consumerbuilder.Build();
+#endif
+            return consumer;
+        }
+    }
+}
---
+++ src/Confluent.Kafka.Core/Producer/Internal/KafkaProducerFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Confluent.Kafka.Core.Producer.Internal
+{
+    internal static class KafkaProducerFactory
+    {
+        public static IKafkaProducer<TKey, TValue> GetOrCreateProducer<TKey, TValue>(
+            IServiceProvider serviceProvider,
+            ILoggerFactory loggerFactory,
+            Action<IServiceProvider, IKafkaProducerBuilder<TKey, TValue>> configureProducer,
+            object producerKey)
+        {
+            var producerbuilder = serviceProvider?.GetKeyedService<IKafkaProducerBuilder<TKey, TValue>>(producerKey) ??
+                new KafkaProducerBuilder<TKey, TValue>()
+                    .WithProducerKey(producerKey)
+                    .WithLoggerFactory(
+                        loggerFactory ??
+                        serviceProvider?.GetService<ILoggerFactory>())
+                    .WithServiceProvider(serviceProvider);
+
+            configureProducer?.Invoke(serviceProvider, producerbuilder);
+
+#if NETSTANDARD2_0_OR_GREATER
+            var producer = producerbuilder.Build().ToKafkaProducer();
+#else
+            var producer = producerbuilder.Build();
+#endif
+            return producer;
+        }
+    }
+}
