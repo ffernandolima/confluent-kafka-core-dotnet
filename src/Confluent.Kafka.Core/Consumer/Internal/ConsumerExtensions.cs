7722e60ae02422477ff1ea0f9b08f79c9b4c9da6
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-06-16 23:32:25 -0300

    Added consumer extensions.

---
+++ src/Confluent.Kafka.Core/Consumer/Internal/ConsumerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Confluent.Kafka.Core.Internal;
+using System;
+
+namespace Confluent.Kafka.Core.Consumer.Internal
+{
+    internal static class ConsumerExtensions
+    {
+        public static IKafkaConsumer<TKey, TValue> ToKafkaConsumer<TKey, TValue>(this IConsumer<TKey, TValue> consumer)
+        {
+            if (consumer is not IKafkaConsumer<TKey, TValue> kafkaConsumer)
+            {
+                throw new InvalidCastException(
+                    $"{nameof(consumer)} should be of type '{typeof(IKafkaConsumer<TKey, TValue>).ExtractTypeName()}'.");
+            }
+
+            return kafkaConsumer;
+        }
+    }
+}
