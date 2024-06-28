33dc8ee36d5c17c8e966bded6136c378cd35007f
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-06-28 18:16:52 -0300

    Added worker diagnostics support.

--- src/Confluent.Kafka.Core.Abstractions/Diagnostics/IDiagnosticsManager.cs
+++ src/Confluent.Kafka.Core.Abstractions/Diagnostics/IDiagnosticsManager.cs
@@ -1,5 +1,7 @@
 ﻿using Confluent.Kafka.Core.Consumer;
+using Confluent.Kafka.Core.Hosting;
 using Confluent.Kafka.Core.Producer;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -26,5 +28,7 @@ namespace Confluent.Kafka.Core.Diagnostics
         void Enrich<TKey, TValue>(Activity activity, DeliveryReport<TKey, TValue> deliveryReport, IKafkaProducerOptions<TKey, TValue> options);
 
         void Enrich<TKey, TValue>(Activity activity, DeliveryResult<TKey, TValue> deliveryResult, IKafkaProducerOptions<TKey, TValue> options);
+
+        void Enrich<TKey, TValue>(Activity activity, ConsumeResult<TKey, TValue> consumeResult, IKafkaConsumerWorkerOptions<TKey, TValue> options, Exception exception = null);
     }
 }
