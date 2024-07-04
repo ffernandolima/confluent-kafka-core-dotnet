9939c7351ccd95c9c04185cb420bb2a69531be0a
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-07-04 16:27:36 -0300

    Added new retry constants.

--- src/Confluent.Kafka.Core/Retry/Internal/KafkaRetryConstants.cs
+++ src/Confluent.Kafka.Core/Retry/Internal/KafkaRetryConstants.cs
@@ -3,5 +3,8 @@
     internal static class KafkaRetryConstants
     {
         public const string RetryHandler = "RetryHandler";
+        public const string RetryCountKey = "X-Retry-Count";
+        public const string RetryGroupIdKey = "X-Retry-Group-Id";
+        public const string RetryTopicSuffix = ".Retry";
     }
 }
