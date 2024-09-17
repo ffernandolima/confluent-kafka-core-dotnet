cabee26b7fc3c444084e3b2d669da1685a2a5ba0
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2024-09-17 18:49:26 -0300

    Added KafkaActivityListener.

---
+++ tests/Confluent.Kafka.Core.Tests/Core/Diagnostics/KafkaActivityListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Confluent.Kafka.Core.Tests.Core.Diagnostics
+{
+    internal static class KafkaActivityListener
+    {
+        public static ActivityListener StartListening(Action<Activity> onListen)
+        {
+            static bool ShouldListenTo(ActivitySource source) => source.Name == "Confluent.Kafka.Core";
+
+            var activityListener = new ActivityListener
+            {
+                ShouldListenTo = ShouldListenTo,
+                SampleUsingParentId = (ref ActivityCreationOptions<string> activityOptions) => ActivitySamplingResult.AllData,
+                Sample = (ref ActivityCreationOptions<ActivityContext> activityOptions) => ActivitySamplingResult.AllData,
+                ActivityStarted = activity => { },
+                ActivityStopped = activity => { onListen?.Invoke(activity); }
+            };
+
+            ActivitySource.AddActivityListener(activityListener);
+
+            return activityListener;
+        }
+    }
+}
